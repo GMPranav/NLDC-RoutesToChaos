2b7aea54544b40ee52f4902f59444407ef090c02
GMP <pranavbtech@gmail.com>
2021-12-06 16:39:38 +0530

    yeboi

---
+++ NLDC-RoutesToChaos/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace NLDC_RoutesToChaos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(115, 67);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(99, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.n_value_changed);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown2.Location = new System.Drawing.Point(115, 105);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(99, 20);
+            this.numericUpDown2.TabIndex = 1;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.x_value_changed);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.AxisX.Title = "Number of Iterations";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.AxisY.Title = "Population";
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(226, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.CustomProperties = "IsXAxisQuantitative=True, EmptyPointValue=Zero";
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Population Value";
+            series1.SmartLabelStyle.IsMarkerOverlappingAllowed = true;
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Single;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(460, 296);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 2;
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown3.Location = new System.Drawing.Point(115, 183);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(99, 20);
+            this.numericUpDown3.TabIndex = 5;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.r_value_changed);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "No. of Iterations (N)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Initial Population";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Rate";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Logistic Map",
+            "Saddle Node Bifurcation",
+            "Hénon mapping"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(170, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.combobox_changed);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(85, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "y(0)";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 2;
+            this.numericUpDown4.Enabled = false;
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown4.Location = new System.Drawing.Point(115, 144);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(99, 20);
+            this.numericUpDown4.TabIndex = 10;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.y_value_changed);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(2, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(193, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Made By: Pranav G (2018AAPS0334H)";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(2, 295);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(225, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Under supervision of: Dr. Aravinda Raghavan ";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 316);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(715, 355);
+            this.Name = "Form1";
+            this.Text = "Routes to Chaos";
+            this.Resize += new System.EventHandler(this.form_resize);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ NLDC-RoutesToChaos/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NLDC_RoutesToChaos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void draw_logis_graph()
+        {
+            foreach (var series in chart1.Series)
+                series.Points.Clear();
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            float x = Convert.ToSingle(numericUpDown2.Value);
+            float r = Convert.ToSingle(numericUpDown3.Value);
+            for (int i = 0; i <= n; i++)
+            {
+                this.chart1.Series["Population Value"].Points.AddXY(i, x);
+                x = x * (1 - x) * r;
+            }
+        }
+
+        public void draw_inter_graph()
+        {
+            foreach (var series in chart1.Series)
+                series.Points.Clear();
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            float x = Convert.ToSingle(numericUpDown2.Value);
+            float r = Convert.ToSingle(numericUpDown3.Value);
+            float max = x;
+            for (int i = 0; i <= n; i++)
+            {
+                x = 1 - (x * x * r);
+                if (x > max)
+                    max = x;
+            }
+            x = Convert.ToSingle(numericUpDown2.Value);
+            for (int i = 0; i <= n; i++)
+            {
+                float xn = x / max;
+                this.chart1.Series["Population Value"].Points.AddXY(i, xn);
+                x = 1 - (x * x * r);
+                if (x < 0)
+                {
+                    break;
+                }
+            }
+
+        }
+
+        public void draw_crisis_graph()
+        {
+            foreach (var series in chart1.Series)
+                series.Points.Clear();
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            float x = Convert.ToSingle(numericUpDown2.Value);
+            float y = Convert.ToSingle(numericUpDown4.Value);
+            float r = Convert.ToSingle(numericUpDown3.Value);
+            float max = x;
+            for (int i = 0; i <= n; i++)
+            {
+                float xt = x;
+                x = 1 - (x * x * r) + y;
+                y = (float)(xt * 0.3);
+                if (x > max)
+                    max = x;
+            }
+            x = Convert.ToSingle(numericUpDown2.Value);
+            y = Convert.ToSingle(numericUpDown4.Value);
+            for (int i = 0; i <= n; i++)
+            {
+                float xn = x / max;
+                float xt = x;
+                this.chart1.Series["Population Value"].Points.AddXY(i, xn);
+                x = 1 - (x * x * r) + y;
+                y = (float)(xt * 0.3);
+                if (x < 0)
+                {
+                    break;
+                }
+            }
+        }
+
+        public void draw_graph()
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                draw_logis_graph();
+                this.chart1.ChartAreas[0].Axes[1].Title = "Population";
+            }
+            else if(comboBox1.SelectedIndex == 1)
+            {
+                draw_inter_graph();
+                this.chart1.ChartAreas[0].Axes[1].Title = "Population (normalized)";
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                draw_crisis_graph();
+                this.chart1.ChartAreas[0].Axes[1].Title = "Population (normalized)";
+            }
+        }
+        public void readjust_graph(int n)
+        {
+            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.ChartAreas[0].AxisX.Crossing = 0;
+            chart1.ChartAreas[0].AxisY.Crossing = 0;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = n;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 1.0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            draw_graph();
+            readjust_graph(n);
+        }
+
+        private void form_resize(object sender, EventArgs e)
+        {
+            int w = this.Width - 255;
+            int h = this.Height - 70;
+            chart1.SetBounds(225, 15, w, h);
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            readjust_graph(n);
+            label5.SetBounds(2, h - 5, 193, 13);
+            label6.SetBounds(2, h + 10, 225, 13);
+        }
+
+        private void n_value_changed(object sender, EventArgs e)
+        {
+            if(numericUpDown1.Value > 1000)
+            {
+                numericUpDown1.Value = 1000;
+            }
+            if (numericUpDown1.Value < 1)
+            {
+                numericUpDown1.Value = 1;
+            }
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            draw_graph();
+            readjust_graph(n);
+        }
+
+        private void x_value_changed(object sender, EventArgs e)
+        {
+            if (numericUpDown2.Value > 1)
+            {
+                numericUpDown2.Value = 1;
+            }
+            if (numericUpDown2.Value < 0)
+            {
+                numericUpDown2.Value = 0;
+            }
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            draw_graph();
+            readjust_graph(n);
+        }
+
+        private void r_value_changed(object sender, EventArgs e)
+        {
+            if (numericUpDown3.Value > 4)
+            {
+                numericUpDown3.Value = 4;
+            }
+            if (numericUpDown3.Value < 0)
+            {
+                numericUpDown3.Value = 0;
+            }
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            draw_graph();
+            readjust_graph(n);
+        }
+
+        private void combobox_changed(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                case 1:
+                    this.label4.ForeColor = SystemColors.ControlDark;
+                    this.numericUpDown4.Enabled = false;
+                    break;
+                case 2:
+                    this.label4.ForeColor = SystemColors.ControlText;
+                    this.numericUpDown4.Enabled = true;
+                    break;
+            }
+        }
+
+        private void y_value_changed(object sender, EventArgs e)
+        {
+            if (numericUpDown4.Value > 1)
+            {
+                numericUpDown4.Value = 1;
+            }
+            if (numericUpDown4.Value < 0)
+            {
+                numericUpDown4.Value = 0;
+            }
+            int n = Convert.ToInt32(numericUpDown1.Value);
+            draw_graph();
+            readjust_graph(n);
+        }
+    }
+}
